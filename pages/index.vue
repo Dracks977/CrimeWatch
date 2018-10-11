fa744347b39bfadac6813a1b3dfe5ef8a150fcc8
Dracks <eclat.nathan@gmail.com>
2018-10-11 17:19:47 +0200

    fixe

--- pages/index.vue
+++ pages/index.vue
@@ -10,26 +10,32 @@
       </h2>
       <div class="links">
         <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
+        href="https://nuxtjs.org/"
+        target="_blank"
+        class="button--green">Documentation</a>
         <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+        href="https://github.com/nuxt/nuxt.js"
+        target="_blank"
+        class="button--grey">GitHub</a>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+  import AppLogo from '~/components/AppLogo.vue'
 
-export default {
-  components: {
-    AppLogo
+  export default {
+    components: {
+      AppLogo
+    },
+    data () {
+      return {
+        name : 'CrimeWatch',
+        description: 'A powerfull Crime encounter'
+      }
+    }
   }
-}
 </script>
 
 <style>

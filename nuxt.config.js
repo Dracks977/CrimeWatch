77180e22af061a921540ee9996b1a858422716ec
Dracks <eclat.nathan@gmail.com>
2018-10-11 22:01:19 +0200

    add register

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,10 +7,11 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '{{escape description }}' }
+      { hid: 'description', name: 'description', content: 'CrimeWatch' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.0.8/css/all.css' }
     ]
   },
   modules: [
--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,147 @@
 <template>
   <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        {{ name }}
-      </h1>
-      <h2 class="subtitle">
-        {{ description }}
-      </h2>
-      <div class="links">
-        <a
-        href="https://nuxtjs.org/"
-        target="_blank"
-        class="button--green">Documentation</a>
-        <a
-        href="https://github.com/nuxt/nuxt.js"
-        target="_blank"
-        class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+    <div class="card bg-light">
+      <article class="card-body mx-auto" style="max-width: 400px;">
+        <h4 class="card-title mt-3 text-center">Create Account</h4>
+        <br>
+        <form>
+          <div class="form-group input-group">
+            <div class="input-group-prepend">
+              <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+            </div>
+            <input v-model='form.name' class="form-control" placeholder="name" type="text">
+          </div> <!-- form-group// -->
+          <div class="form-group input-group">
+            <div class="input-group-prepend">
+              <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+            </div>
+            <input v-model='form.surname' class="form-control" placeholder="surname" type="text">
+          </div> <!-- form-group// -->
+          <div class="form-group input-group">
+            <div class="input-group-prepend">
+              <span class="input-group-text"> <i class="fa fa-envelope"></i> </span>
+            </div>
+            <input v-model='form.email' class="form-control" placeholder="Email address" type="email">
+          </div> <!-- form-group// -->
+          <div class="form-group input-group">
+            <div class="input-group-prepend">
+              <span class="input-group-text"> <i class="fa fa-building"></i> </span>
+            </div>
+            <select v-model='form.rank' class="form-control">
+              <option selected="" value="Agent">Agent</option>
+              <option value="Détective">Détective</option>
+              <option value="Chef de la police">Chef de la police</option>
+            </select>
+          </div> <!-- form-group// -->
+          <div class="form-group input-group">
+            <div class="input-group-prepend">
+              <span class="input-group-text"> <i class="fa fa-clock"></i> </span>
+            </div>
+            <select  v-model='form.seniority' class="form-control">
+             <option>1960</option>
+             <option>1961</option>
+             <option>1962</option>
+             <option>1963</option>
+             <option>1964</option>
+             <option>1965</option>
+             <option>1966</option>
+             <option>1967</option>
+             <option>1968</option>
+             <option>1969</option>
+             <option>1970</option>
+             <option>1971</option>
+             <option>1972</option>
+             <option>1973</option>
+             <option>1974</option>
+             <option>1945</option>
+             <option>1976</option>
+             <option>1977</option>
+             <option>1978</option>
+             <option>1979</option>
+             <option>1980</option>
+             <option>1981</option>
+             <option>1982</option>
+             <option>1983</option>
+             <option>1984</option>
+             <option>1985</option>
+             <option>1986</option>
+             <option>1987</option>
+             <option>1988</option>
+             <option>1989</option>
+             <option>1990</option>
+             <option>1991</option>
+             <option>1992</option>
+             <option>1993</option>
+             <option>1994</option>
+             <option>1995</option>
+             <option>1996</option>
+             <option>1997</option>
+             <option>1998</option>
+             <option>1999</option>
+             <option>2000</option>
+             <option>2001</option>
+             <option>2002</option>
+             <option>2003</option>
+             <option>2004</option>
+             <option>2005</option>
+             <option>2006</option>
+             <option>2007</option>
+             <option>2008</option>
+             <option>2009</option>
+             <option>2010</option>
+             <option>2011</option>
+             <option>2012</option>
+             <option>2013</option>
+             <option>2014</option>
+             <option>2015</option>
+             <option>2016</option>
+             <option>2017</option>
+             <option>2018</option>
+           </select>
+         </div> <!-- form-group end.// -->
+         <div class="form-group input-group">
+          <div class="input-group-prepend">
+            <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+          </div>
+          <input v-model='form.password' class="form-control" placeholder="password" type="password">
+        </div> <!-- form-group// -->                                   
+        <div class="form-group">
+          <div @click='submit()' class="btn btn-primary btn-block"> Create Account  </div>
+        </div> <!-- form-group// -->      
+        <p class="text-center">Have an account? <a href="">Log In</a> </p>                                                                 
+      </form>
+    </article>
+  </div> <!-- card.// -->
+</section>
 </template>
 
 <script>
-  import AppLogo from '~/components/AppLogo.vue'
 
   export default {
-    components: {
-      AppLogo
-    },
-    data () {
+    layout: 'empty',
+    data(){
       return {
-        name : 'CrimeWatch',
-        description: 'A powerfull Crime encounter'
+        form : {
+          name : '',
+          surname : '',
+          email : '',
+          rank : 'Agent',
+          seniority : '1963',
+          activated: 'ko',
+          password : ''
+        }
+      }
+    },
+    mounted: () => {
+
+    },
+    methods: {
+      submit() {
+        this.$axios.post('http://localhost:8000/api/members',this.form).then(response => {
+          alert(response.data)
+        }).catch(e => {
+          alert(e)
+        })
       }
     }
   }
@@ -40,32 +149,8 @@
 
 <style>
 .container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  margin-top: 50vh; /* poussé de la moitié de hauteur de viewport */
+  transform: translateY(-50%); /* tiré de la moitié de sa propre hauteur */
 }
 </style>
 
